897139f17a1f2923c604db5ba3d3490f95ffca4a
bek <shoyatbekov03032003@gmail.com>
2024-09-26 17:16:33 +0500

    /send endpoint added to send sms

--- src/sms/sms.controller.ts
+++ src/sms/sms.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { SmsService } from './sms.service';
+import { SendSmsParams } from './interfaces/send-sms.interface';
 
 @Controller('sms')
 export class SmsController {
-  constructor(private readonly smsService: SmsService) {}
+  constructor(private readonly smsService: SmsService) { }
+
+  @Post('send')
+  async sendSms(@Body() sendSmsParams: SendSmsParams) {
+    return await this.smsService.sendSms(sendSmsParams);
+  }
 }
